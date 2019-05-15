c98aac9f76a3861019876451ace5a67403478047
silvistershebin <silvistershebin@gmail.com>
2019-05-15 12:41:05 +0530

    Offline Feature using SharedPrefernces

--- app/src/main/java/in/creationdevs/aqi/TodayActivity.java
+++ app/src/main/java/in/creationdevs/aqi/TodayActivity.java
@@ -1,6 +1,10 @@
 package in.creationdevs.aqi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -32,6 +36,7 @@ import java.util.Locale;
 
 public class TodayActivity extends AppCompatActivity {
 
+    public static final String PREFERENCES = "VALUES";
     TextView textViewSO2,textViewNO2,textViewPM25,textViewPM10,textViewAQI;
     TextView textViewSO2Desp,textViewNO2Desp,textViewPM25Desp,textViewPM10Desp,textViewAQIDesp;
     String TodayDate;
@@ -69,112 +74,172 @@ public class TodayActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         //Find All Text Views Value
-        textViewSO2=findViewById(R.id.textview_so2);
-        textViewNO2=findViewById(R.id.textview_no2);
-        textViewPM25=findViewById(R.id.textview_pm25);
-        textViewPM10=findViewById(R.id.textview_pm10);
-        textViewAQI=findViewById(R.id.textview_aqi);
+        textViewSO2 = findViewById(R.id.textview_so2);
+        textViewNO2 = findViewById(R.id.textview_no2);
+        textViewPM25 = findViewById(R.id.textview_pm25);
+        textViewPM10 = findViewById(R.id.textview_pm10);
+        textViewAQI = findViewById(R.id.textview_aqi);
 
         //Find All Text View Description
-        textViewSO2Desp=findViewById(R.id.textview_so2_desp);
-        textViewNO2Desp=findViewById(R.id.textview_no2_desp);
-        textViewPM25Desp=findViewById(R.id.textview_pm25_desp);
-        textViewPM10Desp=findViewById(R.id.textview_pm10_desp);
-        textViewAQIDesp=findViewById(R.id.textview_aqi_desp);
-
+        textViewSO2Desp = findViewById(R.id.textview_so2_desp);
+        textViewNO2Desp = findViewById(R.id.textview_no2_desp);
+        textViewPM25Desp = findViewById(R.id.textview_pm25_desp);
+        textViewPM10Desp = findViewById(R.id.textview_pm10_desp);
+        textViewAQIDesp = findViewById(R.id.textview_aqi_desp);
 
 
         //Write code to get today date from the android device
 
-        TodayDate="30-01-2015";
+        TodayDate = "30-01-2015";
 
 
         cal = Calendar.getInstance();
         date = cal.getTime();
-        dateString=dateFormat.format(date);
+        dateString = dateFormat.format(date);
         Toast.makeText(TodayActivity.this, dateFormat.format(date), Toast.LENGTH_SHORT).show();
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES,MODE_PRIVATE);
+        if(sharedPreferences.getString("AQI",null)!=null){
 
+            String so2 = sharedPreferences.getString("SO2",null);
+            textViewSO2.setText(so2);
+            String valso2 = GetDesp(so2);
+            textViewSO2Desp.setText(valso2);
+            String Colso2 = GetColor(valso2);
+            textViewSO2Desp.setTextColor(Color.parseColor("#FFFFFF"));
+            textViewSO2Desp.setBackgroundColor(Color.parseColor(Colso2));
+
+
+            String no2 = sharedPreferences.getString("NO2",null);
+            textViewNO2.setText(no2);
+            String valno2 = GetDesp(no2);
+            textViewNO2Desp.setText(valno2);
+            String Colno2 = GetColor(valno2);
+            textViewNO2Desp.setTextColor(Color.parseColor("#FFFFFF"));
+            textViewNO2Desp.setBackgroundColor(Color.parseColor(Colno2));
+
+
+            String pm25 = sharedPreferences.getString("PM25",null);
+            textViewPM25.setText(pm25);
+            String valpm25 = GetDesp(pm25);
+            textViewPM25Desp.setText(valpm25);
+            String Colpm25 = GetColor(valpm25);
+            textViewPM25Desp.setTextColor(Color.parseColor("#FFFFFF"));
+            textViewPM25Desp.setBackgroundColor(Color.parseColor(Colpm25));
+
+
+            String pm10 = sharedPreferences.getString("PM10",null);
+            textViewPM10.setText(pm10);
+            String valpm10 = GetDesp(pm10);
+            textViewPM10Desp.setText(valpm10);
+            String Colpm10 = GetColor(valpm10);
+            textViewPM10Desp.setTextColor(Color.parseColor("#FFFFFF"));
+            textViewPM10Desp.setBackgroundColor(Color.parseColor(Colpm10));
+
+
+            String aqi = sharedPreferences.getString("AQI",null);
+            textViewAQI.setText(aqi);
+            String valaqi = GetDesp(aqi);
+            textViewAQIDesp.setText(valaqi);
+            String Colaqi = GetColor(valaqi);
+            textViewAQIDesp.setTextColor(Color.parseColor("#FFFFFF"));
+            textViewAQIDesp.setBackgroundColor(Color.parseColor(Colaqi));
+        }
         //Volley Start
-        RequestQueue queue = Volley.newRequestQueue(TodayActivity.this);
-        String url = "http://creationdevs.in/AirIndex/fetch.php";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
+        if (isNetworkAvailable()) {
 
-                //Function Parse JSON
-                try {
-                    JSONArray jsonArray = new JSONArray(response);
-                    int length = jsonArray.length();
-                    String[] s = new String[length];
-                    for (int i = 0; i < length; i++) {
+            RequestQueue queue = Volley.newRequestQueue(TodayActivity.this);
+            String url = "http://creationdevs.in/AirIndex/fetch.php";
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
 
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        String dateget = jsonObject.getString("COL 2");
+                    //Function Parse JSON
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        int length = jsonArray.length();
+                        String[] s = new String[length];
+                        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+                        for (int i = 0; i < length; i++) {
 
-                        if (dateget.equals(dateString)) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String dateget = jsonObject.getString("COL 2");
 
-                            String so2 = jsonObject.getString("COL 8");
-                            textViewSO2.setText(so2);
-                            String valso2 = GetDesp(so2);
-                            textViewSO2Desp.setText(valso2);
-                            String Colso2 = GetColor(valso2);
-                            textViewSO2Desp.setTextColor(Color.parseColor("#FFFFFF"));
-                            textViewSO2Desp.setBackgroundColor(Color.parseColor(Colso2));
-
-
-                            String no2 = jsonObject.getString("COL 9");
-                            textViewNO2.setText(no2);
-                            String valno2 = GetDesp(no2);
-                            textViewNO2Desp.setText(valno2);
-                            String Colno2 = GetColor(valno2);
-                            textViewNO2Desp.setTextColor(Color.parseColor("#FFFFFF"));
-                            textViewNO2Desp.setBackgroundColor(Color.parseColor(Colno2));
+                            if (dateget.equals(dateString)) {
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                String so2 = jsonObject.getString("COL 8");
+                                textViewSO2.setText(so2);
+                                editor.putString("SO2", so2);
+                                String valso2 = GetDesp(so2);
+                                textViewSO2Desp.setText(valso2);
+                                String Colso2 = GetColor(valso2);
+                                textViewSO2Desp.setTextColor(Color.parseColor("#FFFFFF"));
+                                textViewSO2Desp.setBackgroundColor(Color.parseColor(Colso2));
 
 
-                            String pm25 = jsonObject.getString("COL 10");
-                            textViewPM25.setText(pm25);
-                            String valpm25 = GetDesp(pm25);
-                            textViewPM25Desp.setText(valpm25);
-                            String Colpm25 = GetColor(valpm25);
-                            textViewPM25Desp.setTextColor(Color.parseColor("#FFFFFF"));
-                            textViewPM25Desp.setBackgroundColor(Color.parseColor(Colpm25));
+                                String no2 = jsonObject.getString("COL 9");
+                                textViewNO2.setText(no2);
+                                editor.putString("NO2", no2);
+                                String valno2 = GetDesp(no2);
+                                textViewNO2Desp.setText(valno2);
+                                String Colno2 = GetColor(valno2);
+                                textViewNO2Desp.setTextColor(Color.parseColor("#FFFFFF"));
+                                textViewNO2Desp.setBackgroundColor(Color.parseColor(Colno2));
 
 
-                            String pm10 = jsonObject.getString("COL 11");
-                            textViewPM10.setText(pm10);
-                            String valpm10 = GetDesp(pm10);
-                            textViewPM10Desp.setText(valpm10);
-                            String Colpm10 = GetColor(valpm10);
-                            textViewPM10Desp.setTextColor(Color.parseColor("#FFFFFF"));
-                            textViewPM10Desp.setBackgroundColor(Color.parseColor(Colpm10));
+                                String pm25 = jsonObject.getString("COL 10");
+                                textViewPM25.setText(pm25);
+                                editor.putString("PM25", pm25);
+                                String valpm25 = GetDesp(pm25);
+                                textViewPM25Desp.setText(valpm25);
+                                String Colpm25 = GetColor(valpm25);
+                                textViewPM25Desp.setTextColor(Color.parseColor("#FFFFFF"));
+                                textViewPM25Desp.setBackgroundColor(Color.parseColor(Colpm25));
 
 
-                            String aqi = jsonObject.getString("COL 12");
-                            textViewAQI.setText(aqi);
-                            String valaqi = GetDesp(aqi);
-                            textViewAQIDesp.setText(valaqi);
-                            String Colaqi = GetColor(valaqi);
-                            textViewAQIDesp.setTextColor(Color.parseColor("#FFFFFF"));
-                            textViewAQIDesp.setBackgroundColor(Color.parseColor(Colaqi));
+                                String pm10 = jsonObject.getString("COL 11");
+                                textViewPM10.setText(pm10);
+                                editor.putString("PM10", pm10);
+                                String valpm10 = GetDesp(pm10);
+                                textViewPM10Desp.setText(valpm10);
+                                String Colpm10 = GetColor(valpm10);
+                                textViewPM10Desp.setTextColor(Color.parseColor("#FFFFFF"));
+                                textViewPM10Desp.setBackgroundColor(Color.parseColor(Colpm10));
+
+
+                                String aqi = jsonObject.getString("COL 12");
+                                textViewAQI.setText(aqi);
+                                editor.putString("AQI", aqi);
+                                String valaqi = GetDesp(aqi);
+                                textViewAQIDesp.setText(valaqi);
+                                String Colaqi = GetColor(valaqi);
+                                textViewAQIDesp.setTextColor(Color.parseColor("#FFFFFF"));
+                                textViewAQIDesp.setBackgroundColor(Color.parseColor(Colaqi));
+
+                                editor.commit();
+                            }
+
                         }
-
+                    } catch (Exception exe) {
+                        Toast.makeText(TodayActivity.this, "JSON Error" + exe.toString(), Toast.LENGTH_SHORT).show();
                     }
-                } catch (Exception exe) {
-                    Toast.makeText(TodayActivity.this, "JSON Error" + exe.toString(), Toast.LENGTH_SHORT).show();
+
                 }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(TodayActivity.this, "Internet / DataBase Offline", Toast.LENGTH_LONG).show();
 
+                }
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(TodayActivity.this, "Internet / DataBase Offline", Toast.LENGTH_LONG).show();
-
+            );
+            queue.add(stringRequest);
+            //End Volley
+        }
+            else{
+                Toast.makeText(this, "No Internet", Toast.LENGTH_SHORT).show();
             }
         }
-        );
-        queue.add(stringRequest);
-        //End Volley
-    }
+
 
 
     public String GetDesp(String Value)
@@ -263,6 +328,13 @@ public class TodayActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(TodayActivity.this,LineGraph.class);
         startActivity(intent);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
